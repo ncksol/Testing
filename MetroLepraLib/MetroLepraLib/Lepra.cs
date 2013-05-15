c16ed10131f6f8c33198028b5c49b8f45231d289
dobroezlo <nick.sologoub@outlook.com>
2013-05-15 23:01:08 +0100

    1

---
+++ MetroLepraLib/MetroLepraLib/Lepra.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using HtmlAgilityPack;
+
+namespace MetroLepraLib
+{
+    public class Lepra
+    {
+        HttpClient client = new HttpClient();
+
+        private string _captchaUrl;
+        private string _loginCode;
+        
+        public async Task<String> Foo()
+        {
+            var data = await client.GetAsync("http://leprosorium.ru");
+            var dataString = data.Content.ReadAsStringAsync().Result;
+
+            ProcessCaptcha(dataString);
+
+            return _captchaUrl;
+        }
+
+        private void ProcessCaptcha(string data)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(data);
+
+            var captchaImage = htmlDocument.DocumentNode.Descendants().Where(x => x.GetAttributeValue("alt", String.Empty) == "captcha").ToList();
+            if (captchaImage.Count > 0)
+            {
+                _captchaUrl = captchaImage.First().GetAttributeValue("src", String.Empty);
+            }
+
+            var loginCodeInput = htmlDocument.DocumentNode.Descendants().Where(x => x.GetAttributeValue("name", String.Empty) == "logincode").ToList();
+            if (loginCodeInput.Count > 0)
+            {
+                _loginCode = loginCodeInput.First().GetAttributeValue("value", String.Empty);
+            }
+
+        }
+
+        public async void TryLogin(string userName, string password, string captcha)
+        {
+            HttpContent content = new FormUrlEncodedContent(new[]
+                                                                {
+                                                                    new KeyValuePair<string,string>("user", userName),
+                                                                    new KeyValuePair<string,string>("password", password),
+                                                                    new KeyValuePair<string,string>("capture", captcha),
+                                                                    new KeyValuePair<string,string>("logincode", _loginCode),
+                                                                });
+            
+
+
+            var response = client.PostAsync("http://leprosorium.ru/login/", content);
+            
+        }
+    }
+}
---
+++ MetroLepraLib/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using MetroLepraLib;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private Lepra _lepra;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _lepra = new Lepra();
+            var captchaUrl = await _lepra.Foo();
+
+            webBrowser.Source = new Uri("http://leprosorium.ru" + captchaUrl);
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            _lepra.TryLogin(txtUsername.Text, txtPassword.Text, txtCaptcha.Text);
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
